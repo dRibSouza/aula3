e2e97df905bd1f4391be581e620393e2f9ef7f79
dRibSouza <rsouzadanilo@gmail.com>
2023-03-28 19:48:59 -0300

    123

--- src/App.js
+++ src/App.js
@@ -1,35 +1,57 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState , React} from 'react';
 import './App.css';
 import "./style.css"
 
 
+
+
 function App() {
   const [pokemon, setPokemon] = useState([]);
+  const [nomePokemon, setNome] = useState('')
 
+
+  function nome(e) {
+    e.preventDefault();
+    setNome(nomePokemon)
+  }
 
 
   function loadApi() {
     let nomePokemon
-    let url = `https://pokeapi.co/api/v2/pokemon/${nomePokemon}`;
+    
+  }
+  useEffect(() => {
+    loadApi();
+  }, []);
+
+
+  
+  function mostrarPokemon(nome) {
+    let url = `https://pokeapi.co/api/v2/pokemon/${nome}`;
+
     fetch(url)
     .then(response => response.json())
     .then(json => {
         console.log(json)
         setPokemon(json)
     })
-    .catch (err => console.log(err))
+      .catch(err => console.log(err))
   }
-  useEffect(() => {
-    loadApi();
-  }, []);
+
+  
   return (
     <div className="container">
       <header>
         <strong>Pokemon API</strong>
       </header>
       <div>
-        <form typeof='input' placeholder='Pesquise um pokemon'></form>
-        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        
+        <form onSubmit={mostrarPokemon(nomePokemon)}>
+          <input type="text" name='name' value={nomePokemon} onChange={(e) => setNome(e.target.value)} />
+          
+        </form>
+        
+        <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
         <div>Nome: {pokemon.name}</div>
         <div>Nº: {pokemon.id}</div>
         <div>Peso: {pokemon.weight / 10}Kg</div>
@@ -38,5 +60,7 @@ function App() {
     </div>
   );
 }
+// PErguntar sobre imagem, que tenta ser carregada antes de termos pokemon definido e apresenta erra
+// form que envia a informação antes de clicar em submit
 
 export default App;
